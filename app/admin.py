8957319b43c788f80065636f0ae93822a428d91c
olgierrd <oolgierrd@gmail.com>
2024-01-31 16:47:08 +0100

    registered task and tag models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "created_at", "done"]
+    list_filter = ["done"]
+    search_fields = ["content"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
