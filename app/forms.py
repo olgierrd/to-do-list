ea46b69524b60fd88614b51adf181926a15a1663
olgierrd <oolgierrd@gmail.com>
2024-01-31 19:41:29 +0100

    modified tags choice in TaskForm (now multiple choice ticks)

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,11 @@ from app.models import Task
 
 
 class TaskForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Task.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Task
         fields = "__all__"
