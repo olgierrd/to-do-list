15d050d3706e0f0d11584bb378abd0a6aa64026d
olgierrd <oolgierrd@gmail.com>
2024-01-31 16:47:53 +0100

    changed url "home/" --> ""

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app.views import IndexView, TagListView, TaskCreateView
 
 urlpatterns = [
-    path("home/", IndexView.as_view(), name="index"),
+    path("", IndexView.as_view(), name="index"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("tags/", TagListView.as_view(), name="tag-list"),
 ]
